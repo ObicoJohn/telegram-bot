21770b9670866e75940915b4780bd022cd36319d
Chinwuko John Obielue <147950784+ObicoJohn@users.noreply.github.com>
2024-08-04 23:57:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ $(window).on('load', function () {
     $(".preloader").fadeOut(1000);
     $(".container").fadeIn(1000);
 });
+
+//farming button
+
+function changeText() {
+    const button = document.querySelector("#btn");
+    btn.innerHTML = "Farming";
+}
